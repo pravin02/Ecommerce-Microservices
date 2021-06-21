a9357cc95cdc0312977961b57082bad85ddd8c25
Pravin P Patil <prvnpatil11@gmail.com>
2021-06-21 18:56:32 +0530

    port number updated

--- product-catalog-service/src/main/java/org/pk/ecommerce/controllers/CatalogController.java
+++ product-catalog-service/src/main/java/org/pk/ecommerce/controllers/CatalogController.java
@@ -3,6 +3,7 @@ package org.pk.ecommerce.controllers;
 import org.pk.ecommerce.entities.Catalog;
 import org.pk.ecommerce.repositories.CatalogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,16 @@ public class CatalogController {
     @Autowired
     private CatalogRepository repository;
 
+    @Value("${test.message:Hello}")
+    private String message;
+
     @GetMapping
     public List<Catalog> getCatalog() {
         return repository.findAll();
+    }
+
+    @GetMapping("/props")
+    public String getMessage() {
+        return message;
     }
 }
