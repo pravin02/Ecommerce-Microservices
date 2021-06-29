9a2b4c7c23492670aeba03231cbbe1adc54a04e6
Pravin P Patil <prvnpatil11@gmail.com>
2021-06-29 14:26:45 +0530

    testing with @RefreshScope annotation

--- product-catalog-service/src/main/java/org/pk/ecommerce/controllers/CatalogController.java
+++ product-catalog-service/src/main/java/org/pk/ecommerce/controllers/CatalogController.java
@@ -3,20 +3,21 @@ package org.pk.ecommerce.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.pk.ecommerce.entities.Catalog;
 import org.pk.ecommerce.repositories.CatalogRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @Slf4j
 @RestController
 @RequestMapping("/catalog")
 public class CatalogController {
 
-    @Value("${product-catalog.test.prop:Hello}")
+    @Value("${product-catalog.test.prop:test}")
     private String prop;
 
     @Value("${test.message:Hello}")
@@ -35,7 +36,7 @@ public class CatalogController {
 
     @GetMapping("/props")
     public String getMessage() {
-        return message;
+        return this.message;
     }
 
     @GetMapping("/props2")
