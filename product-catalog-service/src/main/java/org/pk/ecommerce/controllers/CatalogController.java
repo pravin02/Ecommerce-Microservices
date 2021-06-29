bb349164379930087b4aac0f4d05c67987c23f7f
Pravin P Patil <prvnpatil11@gmail.com>
2021-06-29 20:10:15 +0530

    config-server testing completed

--- product-catalog-service/src/main/java/org/pk/ecommerce/controllers/CatalogController.java
+++ product-catalog-service/src/main/java/org/pk/ecommerce/controllers/CatalogController.java
@@ -5,6 +5,7 @@ import org.pk.ecommerce.entities.Catalog;
 import org.pk.ecommerce.model.MyProps;
 import org.pk.ecommerce.repositories.CatalogRepository;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
+@RefreshScope
 @Slf4j
 @RestController
 @RequestMapping("/catalog")
