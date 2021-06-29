7347f503d08c56112f8e8c42f826472bbd0f0775
Pravin P Patil <prvnpatil11@gmail.com>
2021-06-29 17:48:00 +0530

    props

--- product-catalog-service/src/main/java/org/pk/ecommerce/controllers/CatalogController.java
+++ product-catalog-service/src/main/java/org/pk/ecommerce/controllers/CatalogController.java
@@ -2,28 +2,31 @@ package org.pk.ecommerce.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.pk.ecommerce.entities.Catalog;
+import org.pk.ecommerce.model.MyProps;
 import org.pk.ecommerce.repositories.CatalogRepository;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RefreshScope
+
 @Slf4j
 @RestController
 @RequestMapping("/catalog")
 public class CatalogController {
 
-    @Value("${product-catalog.test.prop:test}")
-    private String prop;
+    @Value("${test.message:Hello}")
+    private String message;
+
+    private MyProps myProps;
 
     private CatalogRepository repository;
 
-    public CatalogController(CatalogRepository repository) {
+    public CatalogController(CatalogRepository repository, MyProps myProps) {
         this.repository = repository;
+        this.myProps = myProps;
     }
 
     @GetMapping
@@ -32,7 +35,12 @@ public class CatalogController {
     }
 
     @GetMapping("/props")
+    public String getProps() {
+        return this.myProps.getProp();
+    }
+
+    @GetMapping("/message")
     public String getMessage() {
-        return this.prop;
+        return this.message;
     }
 }
--- product-catalog-service/src/main/java/org/pk/ecommerce/model/MyProps.java
+++ product-catalog-service/src/main/java/org/pk/ecommerce/model/MyProps.java
@@ -1,4 +1,15 @@
 package org.pk.ecommerce.model;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@RefreshScope
+@Data
+@Component
+@ConfigurationProperties("product-catalog.test")
 public class MyProps {
+
+    private String prop;
 }
