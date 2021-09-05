6b89403829cd530038f51c4f7dd6d6ab971f7446
Pravin P Patil <prvnpatil11@gmail.com>
2021-09-05 13:21:31 +0530

    props and classes updated

--- ecommerce-admin/src/main/java/org/pk/ecommerce/EcommerceAdminApplication.java
+++ ecommerce-admin/src/main/java/org/pk/ecommerce/EcommerceAdminApplication.java
@@ -3,10 +3,8 @@ package org.pk.ecommerce;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableAdminServer
 public class EcommerceAdminApplication {
 
