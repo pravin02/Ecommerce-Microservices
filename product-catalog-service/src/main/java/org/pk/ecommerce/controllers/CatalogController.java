57e74634f9ce482d5b7847486f086e464a15235a
Pravin P Patil <prvnpatil11@gmail.com>
2021-09-05 11:19:47 +0530

    updated

--- product-catalog-service/src/main/java/org/pk/ecommerce/controllers/CatalogController.java
+++ product-catalog-service/src/main/java/org/pk/ecommerce/controllers/CatalogController.java
@@ -1,9 +1,10 @@
 package org.pk.ecommerce.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+
 import org.pk.ecommerce.entities.Catalog;
-import org.pk.ecommerce.model.MyProps;
 import org.pk.ecommerce.repositories.CatalogRepository;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,23 +23,15 @@ public class CatalogController {
     @Value("${test.message:Hello}")
     private String message;
 
-    private MyProps myProps;
-
     private CatalogRepository repository;
 
-    public CatalogController(CatalogRepository repository, MyProps myProps) {
+    public CatalogController(CatalogRepository repository) {
         this.repository = repository;
-        this.myProps = myProps;
     }
 
     @GetMapping
     public List<Catalog> getCatalog() {
         return repository.findAll();
-    }
-
-    @GetMapping("/props")
-    public String getProps() {
-        return this.myProps.getProp();
     }
 
     @GetMapping("/message")
